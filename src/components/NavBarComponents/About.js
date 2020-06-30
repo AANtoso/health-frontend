970f8af3cd9dbe3657160ca49e3bb627a958ac95
AANtoso <adjoaan@gmail.com>
2020-06-30 13:05:03 -0400

    Moved code to correct file.

---
+++ src/components/NavBarComponents/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Header, Container, Divider } from 'semantic-ui-react';
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                <Divider />
+                <Container testAlign ='justified'>
+                    <Header as='h2'> About diagnosisTracker</Header>
+                    <p>
+                        Diagnosis tracker is an application that can be used to track a patient's diagnosis list and the medications associated with the specific diagnosis.
+                    </p>
+                    <Header as='h3'>How to use diagnosisTracker</Header>
+                    <p> Imput your diagnosis along with its medications and any notes about it.</p>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default About;
