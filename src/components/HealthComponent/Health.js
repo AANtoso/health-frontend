46adf078c399992ab73eb48a2f8b7863ba9b6601
AANtoso <adjoaan@gmail.com>
2020-06-30 12:05:16 -0400

    Added health imports, const, return, and export.

---
+++ src/components/HealthComponent/Health.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { deleteHealth } from '../..actions/healthActions';
+import { Card, Divider, Button, Popup } from 'semantic-ui-react';
+
+const Healths = (props) => {
+    const handleDelete = (health) => {
+        console.log('DELETE HEALTH', health);
+        props.deleteHealth(health.id);
+    };
+
+    return (
+        <div>
+            <Divider />
+            <h4> Your Diagnosis List:</h4>
+            {props.healths.map((hea) => (
+                <Card key={hea.id} color='pink'>
+                    <Link key={hea.id} to={'/healths/${hea.id}'} className='content'>
+                        <center>
+                            <h4>{hea.diagnosis}</h4>
+                        </center>
+                    </Link>
+                    <Popup
+                        content={'Delete "${hea.diagnosis}"'}
+                        size='tiny'
+                        inverted
+                        trigger={
+                            <Button
+                            icon='delete'
+                            size='mini'
+                            attached='bottom'
+                            onClick={() => {
+                                window.confirm(
+                                    'Deleting this condition will delete all of the medications as well! Click ok to confirm.',
+                                ) && handleDelete(hea);
+                            }}
+                            />
+                        }
+                        />
+                </Card>
+            ))}
+        </div>
+    );
+};
+
+export default connect(null, { deleteHealth })(Healths);
