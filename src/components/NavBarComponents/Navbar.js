ba39c39ea7bbb00d510b39cc9c9d85434cc12fc2
AANtoso <adjoaan@gmail.com>
2020-06-30 13:19:44 -0400

    Added Navbar component.

---
+++ src/components/NavBarComponents/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <div className='ui secondary menu'>
+                    <NavLink to='/' className='content'>
+                        <Button color='pink'>Home</Button>
+                    </NavLink>
+
+                    <NavLink to='/about' className='content'>
+                        <Button color='pink'>About</Button>
+                    </NavLink>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
