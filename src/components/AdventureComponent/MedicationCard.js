9e21df5ee7e249687a34ab52a3de19e799ecf098
AANtoso <adjoaan@gmail.com>
2020-06-30 11:07:00 -0400

    Added render function.

--- src/components/AdventureComponent/MedicationCard.js
+++ src/components/AdventureComponent/MedicationCard.js
@@ -3,12 +3,10 @@ import {
     Segment,
     Header,
     Button,
-    Image,
-    Divider,
+    // Divider,
     Icon,
-    Grid,
 } from 'semantic-ui-react';
-import Moment from 'react-moment';
+// import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import {deleteMedication } from '../../actions/medicationActions';
 import MedicationEdit from './MedicationEdit';
@@ -33,5 +31,51 @@ export class MedicationCard extends Component {
         );
     };
 
-    render
+    render() {
+        if (this.state.isEditable) {
+            return (
+                <div>
+                    <MedicationEdit
+                    medication={this.props.medication}
+                    toggle={this.toggleEdit}
+                    />
+                </div>
+            );
+        }
+        return (
+            <div>
+                <Segment color='pink' key={this.props.medication.id}>
+                    <center>
+                        <Header as= 'h2'>{this.props.medication.name}</Header>
+                        <Header as= 'h4'>{this.props.medication.dose}</Header>
+                        <Header as= 'h4'>{this.props.medication.frequency}</Header>
+                        {/* <Moment format='LLLL'>{this.prop.medication.created_at}</Moment> */}
+                    </center>
+                    <br/>
+                    <Button
+                    onClick={this.toggleEdit}
+                    icon 
+                    floated='right'
+                    laabelPosition='left'>
+                        <Icon name='edit' />
+                        Edit
+                    </Button>
+                    
+                    <Button
+                    icon 
+                    laabelPosition='left'
+                    onClick={() => {
+                        window.confirm(
+                            'Are you sure you want to delete this Medication?',
+                        ) && this.handleDelete(this.props.medication);
+                    }}>
+                    <Icon name='delete' />
+                    Delete
+                    </Button>
+                </Segment>
+            </div>
+        );
+    }
 }
+
+export default connect(null, { deleteMedication })(MedicationCard);
