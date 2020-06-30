58674756d00b2a4e03aa66c794ef4c58714e6acb
AANtoso <adjoaan@gmail.com>
2020-06-30 11:48:14 -0400

    Fixed typo and added export.

--- src/components/MedicationComponent/Medications.js
+++ src/components/MedicationComponent/Medications.js
@@ -9,8 +9,10 @@ const Medications = (props) => {
             <h4> Here is a list of medications for this diagnosis:</h4>
             {props.medications &&
                 props.medications.map((med) => (
-                    <MedicationCare key={med.id} medication={med} />
+                    <MedicationCard key={med.id} medication={med} />
                 ))}
         </div>
     );
 };
+
+export default Medications;
