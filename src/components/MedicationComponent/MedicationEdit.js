c845c97d031e33ebe38ccfe15d94931b35d3422f
AANtoso <adjoaan@gmail.com>
2020-06-30 11:30:44 -0400

    Added render function.

--- src/components/MedicationComponent/MedicationEdit.js
+++ src/components/MedicationComponent/MedicationEdit.js
@@ -30,4 +30,55 @@ export class MedicationEdit extends Component {
         this.props.updateMedication(medication);
         this.props.toggle();
     };
+
+    render() {
+        return (
+            <div>
+                <Segment tertiary>
+                    <h3>EDIT</h3>
+                    <Form onSubmit={this.handleUpdate}>
+                        <Form.Group widths='equal'>
+                            <Form.input
+                            type="text"
+                            name='name'
+                            fluid 
+                            label="Name:"
+                            placeholder='Name of the medication.'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            required
+                            />
+                            <Form.input
+                            type="text"
+                            name='dose'
+                            fluid 
+                            label="Dose:"
+                            placeholder='Dose of the medication.'
+                            value={this.state.dose}
+                            onChange={this.handleChange}
+                            required
+                            />
+                            <Form.input
+                            type="text"
+                            name='frequency'
+                            fluid 
+                            label="Frequency:"
+                            placeholder='Frequency to take the medication.'
+                            value={this.state.frequency}
+                            onChange={this.handleChange}
+                            required
+                            />
+                        </Form.Group>
+                        <center>
+                            <Form.Button primary type='submit'>
+                                Save Medication
+                            </Form.Button>
+                        </center>
+                    </Form>
+                </Segment>
+            </div>
+        );
+    }
 }
+
+export default connect(null, { updateMedication })(Medication);
