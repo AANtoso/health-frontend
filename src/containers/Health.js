98cb6abf4c434a354c0b3d02ec1721a43c3a8a00
AANtoso <adjoaan@gmail.com>
2020-06-30 13:48:48 -0400

    Added imports and render.

---
+++ src/containers/Health.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import Healths from '../components/HealthComponent/Healths';
+import HealthShow from '../components/HealthComponent/HealthShow';
+import { connect } from 'react-redux';
+import { fetchHealths } from '../actions/healthActions';
+import { Route, Switch } from 'react-router-dom';
+import { Divider } from 'semantic-ui-react';
+
+class Health extends Component {
+    componentDidMount() {
+        this.props.fetchHealths();
+    }
+
+    render() {
+        return (
+            <div>
+                <Divider />
+                <Switch>
+                    <Route
+                        path='/locations/:id'
+                        render={(routerProps) => {
+                            return (
+                                <HealthShow
+                                {...routerProps}
+                                healths={this.props.healths}
+                            />
+                            );
+                        }}
+                        />
+                        <Route
+                            path='/healths'
+                            render={(routerProps) => {
+                                return (
+                                    <Fragment>
+                                        <Healths
+                                        {...routerProps}
+                                        healths={this.props.healths}
+                                        />
+                                    </Fragment>
+                                );
+                            }}
+                        />
+                </Switch>
+            </div>
+        );
+    }
+}
+
