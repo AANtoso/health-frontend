87f272392c35c0eb8aa9e5b370b07dc52afe5c03
AANtoso <adjoaan@gmail.com>
2020-06-30 12:56:10 -0400

    Added imports and class.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Health from '../src/containers/Health';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/NavbarComponents/Navbar';
+import About from './components/NavbarComponents/About';
+import Home from './components/NavbarComponents/Home';
+
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        App
+      <div className='ui container App'>
+        <br/>
+        <h1> DiagnosisTracker</h1>
+        <Navbar />
+        <Switch>
+          <Routh exact path='/about' component={About}/>
+          <Routh exact path='/' component={Home}/>
+          <Routh exact path='/healths' component={Health}/>
+        </Switch>
       </div>
     );
   }
