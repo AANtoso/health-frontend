c9dd5e44dfaaf246b7e4fc283eeaa05d4b911745
AANtoso <adjoaan@gmail.com>
2020-06-30 10:34:02 -0400

    Added adventures component.

---
+++ src/components/AdventureComponent/Medications.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Divider } from 'semantic-ui-react';
+import MedicationCard from './MedicationCard';
+
+const Medications = (props) => {
+    return (
+        <div>
+            <Divider />
+            <h4> Here is a list of medications for this diagnosis:</h4>
+            {props.medications &&
+                props.medications.map((med) => (
+                    <MedicationCare key={med.id} medication={med} />
+                ))}
+        </div>
+    );
+};
